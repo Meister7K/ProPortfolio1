f051299c102b1f8db5a017e3f4bee8013d578d85
7K <kmanfinkel17@gmail.com>
2023-08-23 23:58:58 -0600

    Adding dist

--- dist/assets/Button-d3412561.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-9803a5a1.js";function r(e){return t.jsx(t.Fragment,{children:e.href===""?t.jsx("button",{className:"btn",children:e.text}):t.jsx("a",{href:e.href,target:"_blank",rel:"noopener noreferrer",children:t.jsx("button",{className:"btn",children:e.text})})})}export{r as B};
