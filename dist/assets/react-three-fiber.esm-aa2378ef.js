1de6135f794eee4f0618a08b31c2a67aa3941e1c
Karl Finkel <kmanfinkel17@gmail.com>
2023-09-19 11:19:30 -0600

    Adding dist

--- dist/assets/react-three-fiber.esm-aa2378ef.js
+++ dist/assets/react-three-fiber.esm-aa2378ef.js
@@ -1,4 +1,4 @@
-import{r as ze,g as a0}from"./index-8cdc9a6b.js";/**
+import{r as ze,g as a0}from"./index-f573fbf0.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
